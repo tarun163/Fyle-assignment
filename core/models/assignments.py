f1c495c16d0e5c8fe37354e04fcd9e3805e5fb96
tarun <2019163@iiitdmj.ac.in>
2022-05-19 11:09:57 +0530

    complete

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -57,7 +57,7 @@ class Assignment(db.Model):
             db.session.add(assignment_new)
 
         db.session.flush()
-        return assignment
+        return assignment       
 
     @classmethod
     def submit(cls, _id, teacher_id, principal: Principal):
@@ -65,13 +65,39 @@ class Assignment(db.Model):
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(assignment.student_id == principal.student_id, 'This assignment belongs to some other student')
         assertions.assert_valid(assignment.content is not None, 'assignment with empty content cannot be submitted')
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT,
+                                    'only a draft assignment can be submitted')
 
         assignment.teacher_id = teacher_id
         assignment.state = AssignmentStateEnum.SUBMITTED
         db.session.flush()
 
         return assignment
+    
+    @classmethod
+    def grades(cls, assignment_new: 'Assignment'):
+        if assignment_new.id is not None:
+            assignment = Assignment.get_by_id(assignment_new.id)
+            assertions.assert_found(assignment, 'No assignment with this id was found')
+            assertions.assert_valid(assignment.teacher_id == assignment_new.teacher_id, 'This assignment belongs to some other teacher')
+            assertions.assert_true(assignment_new.grade == ('A' or 'B' or 'C' or 'D'), 'not valid grade')
+            assertions.assert_valid(assignment.state == AssignmentStateEnum.SUBMITTED,
+                                    'only assignment in submitted state can be graded')
+            
+
+            assignment.grade = GradeEnum(assignment_new.grade)
+            assignment.state = AssignmentStateEnum.GRADED
+        else:
+            assignment = assignment_new
+            db.session.add(assignment_new)
+
+        db.session.flush()
+        return assignment
 
     @classmethod
     def get_assignments_by_student(cls, student_id):
         return cls.filter(cls.student_id == student_id).all()
+    
+    @classmethod
+    def get_assignments_by_teacher(cls, teacher_id):
+        return cls.filter(cls.teacher_id == teacher_id, cls.state == 'SUBMITTED').all()
